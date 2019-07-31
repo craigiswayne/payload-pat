0bcd38d5c99248d7e1c3d460d55bc6a7393518dc
Craig Wayne <craigiswayne@gmail.com>
2019-07-31 15:22:01 +0000

    Update devtools.js

--- assets/scripts/devtools.js
+++ assets/scripts/devtools.js
@@ -1,3 +1,30 @@
+// //test
+//
+// let panel = chrome.devtools.panels.create("Bait", "assets/images/logo.png", "panel.html", function(panel) {
+//
+// });
+//
+// console.log("Panel", panel);
+//
+// chrome.devtools.network.onRequestFinished.addListener(function(result) {
+//
+//     let regEx = `/api/messages/([0-9]{2,3})-([0-9]{4,})`;
+//     if( result.request.url.match(regEx) ){
+//       debugger;
+//     }
+//     chrome.devtools.inspectedWindow.eval('console.log("Request Finished: " + unescape("' +escape(result.request.url) + '"))');
+// });
+//
+// // chrome.webRequest.onBeforeRequest.addListener(function(details) {
+// //
+// //     console.log("Bait", details);
+// //     return {l
+// //       cancel: details.url.indexOf("://www.evil.com/") != -1
+// //     };
+// // },
+// // {urls: ["<all_urls>"]},
+// // ["blocking"]);
+
 chrome.devtools.network.onRequestFinished.addListener(function(request) {
         logger(request.request.url);
 });
@@ -10,30 +37,52 @@ function logger(data){
 
 var PanelWindow;
 
+function requestToDetails(){
+  let accordion = PanelWindow.document.createElement('details');
+  let summary = accordion.appendChild(PanelWindow.document.createElement('summary'));
+  summary.innerHTML = request.request.url;
+  let preText = accordion.appendChild(PanelWindow.document.createElement('pre'));
+  preText.setAttribute("style", "max-height:200px");
+  preText.innerHTML = JSON.stringify(request, undefined, 2);
+  return accordion;
+}
+
 chrome.devtools.network.onRequestFinished.addListener(function(request) {
     if( !PanelWindow ){
       return;
     }
 
-    let preText = PanelWindow.document.body.appendChild(document.createElement('pre'));
-    preText.innerHTML = JSON.stringify(request);
+    const regex = /\/api\/messages\/([0-9]{2,3})-([0-9]{4,})/gm;
 
+    if( !request.request.url.match(regex) ){
+      return;
+    }
+
+    let accordion = PanelWindow.document.createElement('details');
+    let summary = accordion.appendChild(PanelWindow.document.createElement('summary'));
+    summary.innerHTML = request.request.url;
+    let preText = accordion.appendChild(PanelWindow.document.createElement('pre'));
+    preText.setAttribute("style", "max-height:200px");
+    preText.innerHTML = JSON.stringify(request, undefined, 2);
+
+
+
+    PanelWindow.document.body.appendChild(accordion);
     PanelWindow.document.body.appendChild(document.createElement("br"));
-
-    request.getContent(function(c, e){
-        let preText2 = PanelWindow.document.body.appendChild(document.createElement('pre'));
-        preText2.innerHTML = c;
-    })
-
     PanelWindow.document.body.appendChild(document.createElement("hr"));
+
+    // request.getContent(function(c, e){
+    //     let preText2 = PanelWindow.document.body.appendChild(document.createElement('pre'));
+    //     preText2.innerHTML = c;
+    // })
+
+
 });
 
 
-chrome.devtools.panels.create("Pascal", "/assets/images/icon.png", "panel.html",
+chrome.devtools.panels.create("Pascal", "/assets/images/logo.png", "panel.html",
   function(extensionPanel) {
     extensionPanel.onShown.addListener(function(panelWindow) {
         PanelWindow = panelWindow;
     });
 });
-
-
