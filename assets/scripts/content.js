de30f2085ec3d2c19a4ddb9795e1daa5e2a5febd
Craig Wayne <craigiswayne@gmail.com>
2019-08-13 15:06:33 +0000

    Update content.js

--- assets/scripts/content.js
+++ assets/scripts/content.js
@@ -1,12 +1,16 @@
-// content.js
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
+
+    console.log("Message received", request, sender, sendResponse );
+
     if( request.message === "clicked_browser_action" ) {
       var firstHref = document.querySelector("a[href^='http']").getAttribute("href");
-
-      // This line is new!
       chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
-
     }
+
+    if( request.message === 'getSessionInfo' ){
+      chrome.runtime.sendMessage({"message": "sessionInfo", "value": window.sessionStorage} );
+    }
+
   }
 );
